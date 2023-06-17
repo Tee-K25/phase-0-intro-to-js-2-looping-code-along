927f9810131bb684b8e6a147d6c910dc5ece800d
Tony <tony.kiplagat@student.moringaschool.com>
2023-06-17 22:28:19 +0300

    Partially done with loops

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let friends = ["Charlie", "Samip", "Ali"];
+let events = "birthday";
+function writeCards(friends, events) {
+  let messages = [];
+  for (i = 0; i < friends.length; i++) {
+    var message = `Thank you,${friends[i]},for the wonderful ${events} gifts!`;
+    messages.push(message);
+  }
+  return messages;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(`Countdown is ${num}`);
+    num--;
+  }
+}
+countDown(10);
